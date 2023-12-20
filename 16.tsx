b724b8333271b8a7de1b7bb078fc75a674235c5e
大城亮 <hiltoc.katsu@gmail.com>
2023-12-20 20:51:24 +0900

    React関数コンポーネント

---
+++ 16.tsx
@@ -0,0 +1,18 @@
+// React関数コンポーネント
+
+import React from 'react';
+
+// 関数コンポーネントの例
+function MyComponent() {
+  return <div>Hello, React!</div>;
+}
+
+export default MyComponent;
+
+/*関数として記述されるコンポーネントです。
+・関数内でUIを記述し、return文でそのUIを返します。
+・React Hooksを使用して状態やライフサイクルメソッドの機能を実現できます。
+ */
+/*Reactコンポーネントは、Reactライブラリにおいて、UIを構築するための基本的なビルディングブロック。
+コンポーネントは、再利用可能で独立したユニットであり、それぞれが特定の機能やUIの一部を表現します。
+Reactアプリケーションは、これらのコンポーネントの組み合わせによって構築されます。 */
